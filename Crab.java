b23a5ed03e7628f379fc270c29e53d352bcd175a
Wishart <keifer.wishart@stu.boone.kyschools.us>
2021-03-16 12:27:11 -0400

    added Lobster
    Don't you hate it when fish is served with bones

--- Crab.java
+++ Crab.java
@@ -8,27 +8,52 @@ public class Crab extends Actor
     public void act()
     {
     
-        if( isAtEdge())
-        {
-            turn (17);
-            
-        }
-       if(Greenfoot.getRandomNumber(100)<10 )
-       {
-           turn(Greenfoot.getRandomNumber(91) -45 );
-       }
-       move();
        
-       if(isTouching(Worm.class))
-       {
-           removeTouching(Worm.class);
-        }
+        move();
+       lookForWorm();
+       randomTurn();
+       turnAtEdge();
     }
     
     public void move( )
     {
         move(5);
     }
+    /*
+     * Hello this is notes look at them worms it will be eaten
+     */
+  
+    
+    public void lookForWorm()
+    {
+      if(isTouching(Worm.class))
+       {
+           removeTouching(Worm.class);
+        }  
+        
+    }
+    
+    /*
+     * make the crab turn randomly 
+     */
+    public void randomTurn()
+    {
+        if(Greenfoot.getRandomNumber(100)<10 )
+       {
+           turn(Greenfoot.getRandomNumber(91) -45 );
+       } 
+    }
+    
+    public void turnAtEdge()
+    {
+     if( isAtEdge())
+        {
+            turn (17);
+            
+        }   
+        
+    }
+  
 }
 
 
---
+++ Lobster.java
@@ -0,0 +1,62 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Lobster here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lobster extends Actor
+{
+    /**
+     * Act - do whatever the Lobster wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+  public void act()
+    {
+    
+       
+        move();
+       lookForCrab();
+       randomTurn();
+       turnAtEdge();
+    }
+    public void move( )
+    {
+        move(5);
+    }
+    /*
+     * Hello this is notes look at them crab it will be eaten
+     */
+  
+    
+    public void lookForCrab()
+    {
+      if(isTouching(Crab.class))
+       {
+           removeTouching(Crab.class);
+        }  
+        
+    }
+    
+    /*
+     * make the crab turn randomly 
+     */
+    public void randomTurn()
+    {
+        if(Greenfoot.getRandomNumber(100)<10 )
+       {
+           turn(Greenfoot.getRandomNumber(91) -45 );
+       } 
+    }
+    
+    public void turnAtEdge()
+    {
+     if( isAtEdge())
+        {
+            turn (17);
+            
+        }   
+        
+    }
+}
